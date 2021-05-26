09ed85fb7951fd3d4ab94fe270e16162b63ca5ef
jessi68 <lynn312.unist.ac.kr>
2021-05-26 22:32:53 +0900

    apply: VideosView 를 메인 View 에 포함함

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-
+import VideosView from '../src/components/VideosView'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<html lang="kr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>👩🏻‍💻 유튜브 강의실</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="./src/css/index.css" />
+  </head>
+  <body>
+    <div id="app">
+      <div class="d-flex justify-center mt-5 w-100">
+        <div class="w-100">
+          <header class="my-4">
+            <h2 class="text-center font-bold">👩🏻‍💻 나만의 유튜브 강의실 👨🏻‍💻</h2>
+            <nav class="d-flex justify-center">
+              <button class="btn bg-cyan-100 mx-1">👁️ 볼 영상</button>
+              <button class="btn mx-1">✅ 본 영상</button>
+              <button id="search-button" class="btn mx-1">
+                🔍 동영상 검색
+              </button>
+            </nav>
+          </header>
+        </div>
+      </div>
+      <div class="modal">
+        <div class="modal-inner p-8">
+          <button class="modal-close">
+            <svg viewbox="0 0 40 40">
+              <path class="close-x" d="M 10,10 L 30,30 M 30,10 L 10,30" />
+            </svg>
+          </button>
+          <header>
+            <h2 class="text-center">🔎 유튜브 검색</h2>
+          </header>
+          <form class="d-flex">
+            <input type="text" class="w-100 mr-2 pl-2" placeholder="검색" />
+            <button type="button" class="btn bg-cyan-500">검색</button>
+          </form>
+          <section class="mt-2">
+            <span class="text-gray-700">최근 검색어: </span>
+          </section>
+          <section>
+            <div class="d-flex justify-end text-gray-500">
+              저장된 영상 갯수: 0개
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
+     <VideosView/>
+    <script type="module" src="./src/js/index.js"></script>
+  </body>
+</html>
   );
 }
 
